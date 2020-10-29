352cc708edd11317e767ecb092faf94a3ae55bb6
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-10-29 15:51:59 +0100

    Function implementations

--- test/keyEvent/KeyEventListener.cpp
+++ test/keyEvent/KeyEventListener.cpp
@@ -1,5 +1,15 @@
 #include "KeyEventListener.h"
+#include <Event.h>
 
 namespace cae
 {
+	KeyEventListener::KeyEventListener(Event& iEventNum, Event& iEventStr)
+	{
+	}
+	void KeyEventListener::onNumber(const EventArgs& iArgs)
+	{
+	}
+	void KeyEventListener::onString(const EventArgs& iArgs)
+	{
+	}
 }
