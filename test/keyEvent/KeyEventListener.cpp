ba7c17a4ede40dd603320d26654fa6504892c5de
KissG549 <57135398+KissG549@users.noreply.github.com>
2020-10-31 22:32:07 +0100

    Implemented KeyEventArgs and KeyEventListener functs

--- test/keyEvent/KeyEventListener.cpp
+++ test/keyEvent/KeyEventListener.cpp
@@ -2,13 +2,66 @@
 
 namespace cae
 {
-	KeyEventListener::KeyEventListener(Event& iEventNum, Event& iEventStr)
+	template<typename T>
+	void KeyEventArgs<T>::setValue(const T iVal)
 	{
+		mValue = iVal;
 	}
+
+	template<typename T>
+	const T& KeyEventArgs<T>::getValue()
+	{
+		return mValue;
+	}
+
+	template<typename T>
+	const Event& KeyEventArgs<T>::getEvent()
+	{
+		return mSrcEvent;
+	}
+
+	KeyEventListener::KeyEventListener(Event& iEventNum, Event& iEventStr)
+		:mEventNum(iEventNum),mEventStr(iEventStr)
+	{
+		iEventNum += *this;
+		iEventStr += *this;
+	}
+
+	void KeyEventListener::operator()(const EventArgs& iArgs)
+	{
+		std::cout << "KeyEventListener::operator called" << std::endl;
+		EventArgs *tmpArgs = const_cast<EventArgs *>(&iArgs);
+		KeyEventArgs<std::string>* strEventArgs = dynamic_cast<KeyEventArgs<std::string>*>(tmpArgs);
+
+		if ( strEventArgs )
+		{
+			// compare addresses
+			if (&(strEventArgs->getEvent()) == &mEventStr)
+			{	// it's a string
+				onString(iArgs);
+			}
+			else
+			{
+				// it's a number
+				onNumber(iArgs);
+			}
+		}
+	}
+
 	void KeyEventListener::onNumber(const EventArgs& iArgs)
 	{
+		EventArgs* tmpArgs = const_cast<EventArgs*>(&iArgs);
+		KeyEventArgs<std::string>* strEventArgs = dynamic_cast<KeyEventArgs<std::string>*>(tmpArgs);
+		
+		std::cout << "KeyEventListener::onNumber " << strEventArgs->getValue() << std::endl;
 	}
+
 	void KeyEventListener::onString(const EventArgs& iArgs)
 	{
+		EventArgs* tmpArgs = const_cast<EventArgs*>(&iArgs);
+		KeyEventArgs<std::string>* strEventArgs = dynamic_cast<KeyEventArgs<std::string>*>(tmpArgs);
+
+		std::cout << "KeyEventListener::onString " << strEventArgs->getValue() << std::endl;
 	}
+
 }
